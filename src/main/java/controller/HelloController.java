cbd9ade7e1b997678a9d7d7838dd3f023fc0ab3c
WatashiwaToki <Tramanamacon@gmail.com>
2023-06-12 11:31:17 -0500

    early setup of project

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,4 +1,4 @@
-package hughes.scheduleproject;
+package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module hughes.scheduleproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens hughes.scheduleproject to javafx.fxml;
     exports hughes.scheduleproject;
+    exports controller;
+    opens controller to javafx.fxml;
 }
